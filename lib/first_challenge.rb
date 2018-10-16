7bc7d3a22381416342604da230782c24984758f7
Emily Asch <e.asch@me.com>
2018-10-16 21:26:01 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,12 +15,15 @@ def first_challenge
 
   #your code here
 contacts.each do |person, data|
-  data.each do |attribute, value|
-    :favorite_icecream_flavors.delete_if {|value| value == "strawberry"}
+  data.each do |key, value|
+    if key == :favorite_icecream_flavors
+      value.delete_if do |flavor|
+        flavor == "strawberry"
 end
 end
 
   #remember to return your newly altered contacts hash!
+  contacts
   contacts
 end
 
